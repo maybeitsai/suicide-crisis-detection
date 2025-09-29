cee640ecfc4468a2604d9bfb914c854d4612cc56
maybeitsai <harrymardika.ai@gmail.com>
2025-09-29 14:11:53 +0700

    Set number of threads for PyTorch to improve performance

--- run_model.py
+++ run_model.py
@@ -12,6 +12,7 @@ from PIL import Image
 # Setup device
 # ======================
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+torch.set_num_threads(4)
 
 # ======================
 # Load Face Expression Model
